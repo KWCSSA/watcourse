c1f1593975f1ea4778572c8f12eb0c7358267bc1
Tianyu Wang <terrywang@tianyu.ninja>
2019-11-08 17:56:00 -0500

    Changed App to class component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -10,23 +10,25 @@ const CourseList = () => <h1>Course List</h1>;
 const BookMarkList = () => <h1>Book Mark List</h1>;
 const NotFound404 = () => <h1>404 Not Found</h1>;
 
-function App() {
-	return (
-		<div className='app-root'>
-			<div className='app-title'>WATCOURSE</div>
-			<BrowserRouter>
-				<div className='app-area'>
-					<Switch>
-						<Route exact path='/' component={Home} />
-						<Route exact path='/courses' component={CourseList} />
-						<Route exact path='/bookmarks' component={BookMarkList} />
-						<Route component={NotFound404} />
-					</Switch>
-				</div>
-				<NavBar />
-			</BrowserRouter>
-		</div>
-	);
+class App extends React.Component {
+	render() {
+		return (
+			<div className='app-root'>
+				<div className='app-title'>WATCOURSE</div>
+				<BrowserRouter>
+					<div className='app-area'>
+						<Switch>
+							<Route exact path='/' component={Home} />
+							<Route exact path='/courses' component={CourseList} />
+							<Route exact path='/bookmarks' component={BookMarkList} />
+							<Route component={NotFound404} />
+						</Switch>
+					</div>
+					<NavBar />
+				</BrowserRouter>
+			</div>
+		);
+	}
 }
 
 export default App;
