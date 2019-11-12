efcb87834fa069cbda055d508a1198fdc717237f
Tianyu Wang <terrywang@tianyu.ninja>
2019-11-12 04:21:44 -0500

    Added instruction link to home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import '../../css/home/home.css';
 
@@ -6,7 +7,13 @@ function Home(props) {
 	return (
 		<div className='home-root'>
 			<img className='home-app-logo' src='/images/logo.png' alt='WATCOURSE logo' />
-			<h5 style={{ textAlign: 'center' }}>请使用查询页面查看课程详情信息</h5>
+			<h5 style={{ textAlign: 'center', marginBottom: '20px' }}>请使用查询页面查看课程详情信息</h5>
+			<Link to={'/addtohome/ios'} className='home-app-addtohome'>
+				<img src='/images/iosAddToHome/safari.svg' alt='Safari' className='home-app-addtohome-icon' /> 添加到桌面
+			</Link>
+			<Link to={'/addtohome/chrome'} className='home-app-addtohome'>
+				<img src='/images/chromeAddToHome/chrome.svg' alt='Chrome' className='home-app-addtohome-icon' /> 添加到桌面
+			</Link>
 		</div>
 	);
 }
